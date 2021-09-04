bc0f7cab3e341c1c15bc3ee72dbcf1b926af1b48
Anthony Lifu <tonylifu@outlook.com>
2021-09-04 20:08:12 +0100

    Add the use Spring JMS

--- src/main/java/com/pangea/pub/test/controller/PublishController.java
+++ src/main/java/com/pangea/pub/test/controller/PublishController.java
@@ -1,10 +1,13 @@
 package com.pangea.pub.test.controller;
 
 import com.pangea.pub.test.dto.request.PublishRequest;
+import com.pangea.pub.test.jms.JMSService;
 import com.pangea.pub.test.service.PublishService;
+import com.pangea.pub.test.util.AppConstant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +18,12 @@ public class PublishController {
     private static final Logger LOG = LoggerFactory.getLogger(PublishController.class);
 
     private PublishService publishService;
+    private JMSService jmsService;
 
     @Autowired
-    public PublishController(PublishService publishService) {
+    public PublishController(@Qualifier("publishServiceImpl") PublishService publishService, JMSService jmsService) {
         this.publishService = publishService;
+        this.jmsService = jmsService;
     }
 
     @PostMapping("/{topic}")
@@ -27,8 +32,23 @@ public class PublishController {
         LOG.info("\n\nPublish RequestData:\n{}\n\n", requestData);
         PublishRequest publishRequest = getPublishRequest(topic, requestData);
         LOG.info("\n\nPublish Request:\n{}\n\n", publishRequest);
-        publishService.publishToTopic(topic, publishRequest);
+        //publishService.publishToTopic(topic, publishRequest);
+        //jmsService.jmsServiceSendToTopicOne(topic, publishRequest);
+        publishToTopic(topic, publishRequest);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private void publishToTopic(String topic, PublishRequest publishRequest) {
+        switch (topic){
+            case AppConstant.TOPIC_ONE:
+                jmsService.jmsServiceSendToTopicOne(topic, publishRequest);
+                break;
+            case AppConstant.TOPIC_TWO:
+                jmsService.jmsServiceSendToTopicTwo(topic, publishRequest);
+                break;
+            default:
+                publishService.publishToTopic(topic, publishRequest);
+        }
     }
 
     private PublishRequest getPublishRequest(String topic, Object requestData) {
--- src/main/java/com/pangea/pub/test/util/AppConstant.java
+++ src/main/java/com/pangea/pub/test/util/AppConstant.java
@@ -4,9 +4,13 @@ import com.pangea.pub.test.dto.response.SubscriptionResponse;
 import com.pangea.pub.test.model.Subscription;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class AppConstant {
+    public static final String TOPIC_ONE = "topic1";
+    public static final String TOPIC_TWO = "topic2";
     private static HashMap<String, Subscription> subscriptions = new HashMap<>();
+//    private static DestionationTopic destination = new DestionationTopic();
 
     public static SubscriptionResponse createSubscription(String topic, String url) {
         String key = topic+url;
@@ -21,4 +25,23 @@ public class AppConstant {
     public static HashMap<String, Subscription> getSubscriptions() {
         return subscriptions;
     }
+
+    public static HashMap<String, Subscription> getSubscribersList(String topic, HashMap<String, Subscription> subscriptions){
+        HashMap<String, Subscription> subscribers = new HashMap<>();
+        for (Map.Entry<String, Subscription> entry : subscriptions.entrySet()) {
+            String topicIn = entry.getValue().getTopic();
+            if(topicIn.trim().equalsIgnoreCase(topic.trim())){
+                subscribers.put(entry.getKey(), entry.getValue());
+            }
+        }
+        return  subscribers;
+    }
+
+//    public static DestionationTopic getDestination(){
+//        return destination;
+//    }
+//
+//    public static void setDestination(DestionationTopic dest){
+//        destination.setTopic(dest.getTopic());
+//    }
 }
